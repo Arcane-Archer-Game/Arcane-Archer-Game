22848f06f6b34d762106d3336828d84785a209b6
friend1226 <friendcha7749@gmail.com>
2025-05-14 15:29:51 +0900

    [restore] 이전 작업물 복구 및 병합

---
+++ Arcane_Archer/Source/Arcane_Archer/Arcane_Archer.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Arcane_Archer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Arcane_Archer, "Arcane_Archer" );
+ 
---
+++ Arcane_Archer/Source/Arcane_Archer/Arcane_ArcherGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Arcane_ArcherGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AArcane_ArcherGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AArcane_ArcherGameMode();
+};
+
+
+
